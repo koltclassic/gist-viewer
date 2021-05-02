6891e3bbac8e2283b0e08f68af253863f65798f1
koltclassic <koltewing@gmail.com>
2021-05-02 10:59:45 -0400

    add basic routing for gist

--- src/App.js
+++ src/App.js
@@ -1,50 +1,30 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import { getGistsForUser } from './api/gists';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home';
+import Gist from './components/Gist';
 
 function App() {
-
-  const [usernameToSearch, setUsernameToSearch] = useState('');
-  const [usernameForQuery, setUsernameForQuery] = useState('');
-  const [searchedUserGists, setSearchedUserGists] = useState(null);
-
-  useEffect(() => {
-    const getUserGistData = async () => {
-      const userGists = await getGistsForUser(usernameForQuery);
-      if (userGists && userGists.data) {
-        console.log({ userGists })
-        setSearchedUserGists(userGists.data);
-      }
-    }
-
-    if (usernameForQuery) {
-      getUserGistData();
-    }
-  }, [usernameForQuery]);
-
-
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <main>
-        <label htmlFor="searchUser">Search username for gists</label>
-        <input type="text" id="searchUser" value={usernameToSearch} onChange={(e) => setUsernameToSearch(e.target.value)} />
-        <button onClick={() => setUsernameForQuery(usernameToSearch)}>Search</button>
-      </main>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+        </header>
 
-      <div>username: {usernameToSearch}</div>
-      <div>
-        {searchedUserGists && searchedUserGists.map((gist) => {
-          return (
-            <div>
-              <span>{gist.description && gist.description}</span>
-              <span>{gist.created_at}</span>
-            </div>
-          )
-        })}
+        <Switch>
+          <Route path="/:gistId">
+            <Gist />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 

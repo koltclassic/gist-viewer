822616d444898b6a3c74dd72e9ba5452324e880a
koltclassic <koltewing@gmail.com>
2021-05-02 11:09:16 -0400

    update Gist component with getGistByID request

--- src/components/Gist/index.js
+++ src/components/Gist/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { getGistByID } from '../../api/gists';
 
 const Gist = () => {
   let { gistId } = useParams();
+  const [gistData, setGistData] = useState(null);
+
+  useEffect(() => {
+    const getGistData = async () => {
+      const gist = await getGistByID(gistId);
+      if (gist && gist.data) {
+        setGistData(gist.data);
+      }
+    }
+
+    getGistData();
+  }, [gistId]);
+
   return (
-    <div>Gist with id: {gistId}</div>
+    <pre>{JSON.stringify(gistData, undefined, 2)}</pre>
   )
 }
 
